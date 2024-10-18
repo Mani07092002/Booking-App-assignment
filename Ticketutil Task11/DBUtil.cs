4d27b98fc4c15676912c0b185c0ded39c8c523cb
MOGALLAPALLI MANIKANTA <140966613+Mani07092002@users.noreply.github.com>
2024-10-18 14:55:02 +0530

    Add files via upload

---
+++ Ticketutil Task11/DBUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace Ticketutil
+{
+    public static class DBUtil
+    {
+        public static string GetConnectionString()
+        {
+            return "Data Source=DESKTOP-295VFEG\\SQLEXPRESS;Initial Catalog=Ticket;Integrated Security=True;Trust Server Certificate=True;";
+        }
+
+
+        public static SqlConnection GetDBConn()
+        {
+            SqlConnection conn = new SqlConnection(GetConnectionString());
+            conn.Open(); 
+            return conn; 
+        }
+    }
+}
+
